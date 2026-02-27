b9f11664021dd5627a72b2a3f5edf6f626bb582d
Rasmus <rasmus@outlandish.com>
2026-02-27 17:20:50 +0000

    bringing more up to date by using wordpress's own scss files to generate the css

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-wp-standout-staging
+# wp-standout-staging
+
+Simple plugin for making the admin bars of local/staging sites stand out
+
+css compiled from scss files with
+`sass --no-source-map --load-path=[path to wp folder] [path to plugin folder]/css/`
--- standout-staging.php
+++ standout-staging.php
@@ -11,21 +11,15 @@ Author URI: http://outlandish.com
 License: GPL2
 */
 
-add_action('wp_enqueue_scripts', function () {
-    $publicRoot = plugin_dir_url(__FILE__) . '/css/';
-    if (strpos($_SERVER['WP_HOME'], 'local')) {
-        wp_enqueue_style('staging-css', $publicRoot . 'development.css');
-    } elseif (strpos($_SERVER['WP_HOME'], 'staging')) {
-        wp_enqueue_style('development-css', $publicRoot . 'staging.css');
+function enqueueStandoutCss()
+{
+    $isLocal = str_contains($_SERVER['WP_HOME'], 'local');
+    $isStaging = str_contains($_SERVER['WP_HOME'], 'staging');
+    if ($isLocal || $isStaging) {
+        $publicRoot = plugin_dir_url(__FILE__) . '/css/';
+        wp_enqueue_style('standout-css-vars', $publicRoot . ($isLocal ? 'development' : 'staging') . '.css');
     }
-});
+}
 
-
-add_action('admin_enqueue_scripts', function () {
-    $publicRoot = plugin_dir_url(__FILE__) . '/css/';
-    if (strpos($_SERVER['WP_HOME'], 'local')) {
-        wp_enqueue_style('staging-css', $publicRoot . 'development.css');
-    } elseif (strpos($_SERVER['WP_HOME'], 'staging')) {
-        wp_enqueue_style('development-css', $publicRoot . 'staging.css');
-    }
-});
+add_action('wp_enqueue_scripts', 'enqueueStandoutCss');
+add_action('admin_enqueue_scripts', 'enqueueStandoutCss');
