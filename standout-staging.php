105ec8ca5579d35995bc7ae7c093675a20e49349
Rasmus <rasmus@outlandish.com>
2026-07-28 14:56:01 +0100

    updating link colours

--- standout-staging.php
+++ standout-staging.php
@@ -5,7 +5,7 @@ description:
 Make staging and development sites clearly visible.
 If the domain has 'local' in it, show a blue wordpress admin bar.
 If the domain has 'staging' in it, show a red wordpress admin bar.
-Version: 0.1
+Version: 0.2
 Author: Konstantinos Kokkorogiannis
 Author URI: http://outlandish.com
 License: GPL2
